67a6024fcf07e5e7ff1bc2e6568cf7e749817ff1
John <Jcastro091@aol.com>
2014-10-06 15:19:46 -0400

    9 patch, UI

---
+++ src/com/bignerdranch/android/remotecontrol/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.bignerdranch.android.remotecontrol;
+
+import com.bignerdranch.android.remotecontrol.R;
+import com.bignerdranch.android.remotecontrol.R.id;
+import com.bignerdranch.android.remotecontrol.R.layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * @author John
+ * 
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+	protected abstract Fragment createFragment();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragment);
+
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+		if (fragment == null) {
+			fragment = createFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment)
+					.commit();
+		}
+	}
+
+}
